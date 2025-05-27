ffa73481765c54aedeb68986b3fd5b46cfec8549
KulichO <oaosokina@yandex.ru>
2025-05-27 21:08:16 +0300

    13

--- block7/src/js/index.js
+++ block7/src/js/index.js
@@ -1,21 +1,43 @@
 import '../scss/style.scss'
 
-let showButton = document.querySelector('.button--show');
-let textChange = document.querySelector('.next--show');
-let list = document.querySelector('.brands__list');
+let brandsShowButton = document.querySelector('.button--show');
+let brandsTextChange = document.querySelector('.next--show');
+let brandsList = document.querySelector('.brands__list');
 
-showButton.addEventListener ('click', function(){
-    showButton.classList.toggle('button--hide');
-    textChange.classList.toggle('next--hide');
-    list.classList.toggle('brands__list--show');
+brandsShowButton.addEventListener ('click', function(){
+    brandsShowButton.classList.toggle('button--hide');
+    brandsTextChange.classList.toggle('next--hide');
+    brandsList.classList.toggle('brands__list--show');
     
-    if (showButton.classList.contains('button--hide')) {
-        showButton.textContent = 'Скрыть';
+    if (brandsShowButton.classList.contains('button--hide')) {
+        brandsShowButton.textContent = 'Скрыть';
     } else {
-        showButton.textContent = 'Показать все';
+        brandsShowButton.textContent = 'Показать все';
     }
 
 });
+
+let typesShowButton = document.querySelector('.button-types--show');
+let typesTextChange = document.querySelector('.types--show');
+let typesList = document.querySelector('.types__list');
+
+typesShowButton.addEventListener ('click', function(){
+    typesShowButton.classList.toggle('button-types--hide');
+    typesTextChange.classList.toggle('types--hide');
+    typesList.classList.toggle('types__list--show');
+    
+    if (typesShowButton.classList.contains('button-types--hide')) {
+        typesShowButton.textContent = 'Скрыть';
+    } else {
+        typesShowButton.textContent = 'Показать все';
+    }
+
+});
+
+
+
+
+
 
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',

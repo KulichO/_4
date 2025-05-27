8ebcbbede6305e658bb6e75852b701eac9e8aea8
KulichO <oaosokina@yandex.ru>
2025-05-27 16:57:40 +0300

    9

--- block7/src/js/index.js
+++ block7/src/js/index.js
@@ -1,3 +1,30 @@
 import '../scss/style.scss'
 
-console.log('Works!')
+let showButton = document.querySelector('.button--show');
+let textChange = document.querySelector('.next--show');
+let list = document.querySelector('.brands__list');
+
+showButton.addEventListener ('click', function(){
+    showButton.classList.toggle('button--hide');
+    textChange.classList.toggle('next--hide');
+    list.classList.toggle('brands__list--show');
+    
+    if (showButton.classList.contains('button--hide')) {
+        showButton.textContent = 'Скрыть';
+    } else {
+        showButton.textContent = 'Показать все';
+    }
+
+});
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1.2,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  })
